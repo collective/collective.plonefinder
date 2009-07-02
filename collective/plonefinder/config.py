1677ddbc1d43f82fff9f97113bc7d93f8149aed1
jean-mat Grimaldi <jeanmat.grimaldi@gmail.com>
2009-07-02 08:09:21 +0000

    collective.plonefinder first import. ajax widget for referencing contents (plonearticle explorer in a separate package)
    git-svn-id: https://svn.plone.org/svn/collective/collective.plonefinder/trunk@89117 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/plonefinder/config.py
@@ -0,0 +1,8 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'collective.plonefinder'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
---
+++ collective/plonefinder/interfaces/finder.py
@@ -0,0 +1,4 @@
+from zope.interface import Interface
+
+class IFinder(Interface):
+    """Plone finder view"""
