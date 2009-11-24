513c952aff73feb8266c217e25f9a285ff16f688
jean-mat Grimaldi <jeanmat.grimaldi@gmail.com>
2009-11-24 18:15:27 +0000

    resolve problem with collective.uploadify and Authentification, using a ticket to simulate authentification for flash form (code taken from PloneFlashUpload)
    git-svn-id: https://svn.plone.org/svn/collective/collective.plonefinder/trunk@103208 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/plonefinder/browser/finder.py
+++ collective/plonefinder/browser/finder.py
@@ -1,6 +1,10 @@
+
+import mimetypes
 from ZTUtils import make_query
 from zope.component import getMultiAdapter
 from zope.interface import implements
+from zope.filerepresentation.interfaces import IFileFactory
+
 from Products.Five import BrowserView
 from Acquisition import aq_base, aq_inner
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
@@ -22,12 +26,6 @@ def _quote_bad_chars(s):
         s = s.replace(char, _quotestring(char))
     return s
 
-
-def encode(s):
-    """ encode string
-    """
-
-    return "d".join(map(str, map(ord, s)))
 
 FORM_PARAMS = ('SearchableText',)                                                
 
@@ -433,76 +431,7 @@ class Finder(BrowserView):
         """
         make a query_string with clean Request
         """
-        
-        #cleanquery = make_query(self.cleanrequest).replace('%20', '+')        
+           
         return make_query(self.cleanrequest)             
         
-class FinderUploadView(BrowserView):
-    """ The Finder Upload View
-    """
-
-    template = ViewPageTemplateFile("finder_upload.pt")
-
-    def __call__(self):
-        return self.template()
-
-FINDER_UPLOAD_JS = """
-    jQuery(document).ready(function() {
-        jQuery('#uploader').uploadify({
-            'uploader'      : '%(portal_url)s/++resource++uploader.swf',
-            'script'        : '%(context_url)s/@@upload_file',
-            'cancelImg'     : '++resource++cancel.png',
-            'folder'        : '%(physical_path)s',
-            'scriptData'    : {'cookie': '%(cookie)s'},
-            'onAllComplete' : Browser.onUploadComplete,
-            'auto'          : %(ul_auto_upload)s,
-            'multi'         : %(ul_allow_multi)s,
-            'simUploadLimit': '%(ul_sim_upload_limit)s',
-            'sizeLimit'     : '%(ul_size_limit)s',
-            'fileDesc'      : '%(ul_file_description)s',
-            'fileExt'       : '%(ul_file_extensions)s',
-            'buttonText'    : '%(ul_button_text)s',
-            'buttonImg'     : '%(ul_button_image)s',
-            'scriptAccess'  : '%(ul_script_access)s',
-            'hideButton'    : %(ul_hide_button)s
-        });
-    });
-"""
-
         
-class FinderUploadInit(BrowserView):
-    """ Initialize uploadify js
-    """
-
-    def __init__(self, context, request):
-        super(FinderUploadInit, self).__init__(context, request)
-        self.context = aq_inner(context)
-
-    def upload_settings(self):
-        sp = getToolByName(self.context, "portal_properties").site_properties
-        portal_url = getToolByName(self.context, 'portal_url')
-
-        settings = dict(
-            cookie              = self.request.cookies.get('__ac', ''),
-            portal_url          = portal_url(),
-            context_url         = self.context.absolute_url(),
-            physical_path       = "/".join(self.context.getPhysicalPath()),
-            ul_auto_upload      = sp.getProperty('ul_auto_upload', 'true'),
-            ul_allow_multi      = sp.getProperty('ul_allow_multi', 'true'),
-            ul_sim_upload_limit = sp.getProperty('ul_sim_upload_limit', 4),
-            ul_size_limit       = sp.getProperty('ul_size_limit', ''),
-            ul_file_description = sp.getProperty('ul_file_description', ''),
-            ul_file_extensions  = sp.getProperty('ul_file_extensions', '*.*;'),
-            ul_button_text      = sp.getProperty('ul_button_text', 'BROWSE'),
-            ul_button_image     = sp.getProperty('ul_button_image', ''),
-            ul_hide_button      = sp.getProperty('ul_hide_button', 'false'),
-            ul_script_access    = sp.getProperty('ul_script_access', 'sameDomain'),
-        )
-        
-        settings["cookie"] = encode(settings["cookie"])
-        
-        return settings
-
-    def __call__(self):
-        settings = self.upload_settings()
-        return FINDER_UPLOAD_JS % settings        
