94f6f98e0408edf767c073dba27a01fb98ef8cf5
Gilles Lenfant <gilles.lenfant@gmail.com>
2011-02-13 15:14:07 +0000

    Massive optims in finder and lots of FIXME added

--- collective/plonefinder/utils.py
+++ collective/plonefinder/utils.py
@@ -5,13 +5,14 @@
 from Products.CMFPlone.utils import getSiteEncoding
 
 def pleaseDontCache(context, request):
-    """Asks the proxy (if any) and user browser to avoid caching the response
+    """Requests the proxy (if any) and user browser to avoid caching the response
     """
     charset = getSiteEncoding(context)
     setHeader = request.RESPONSE.setHeader
-    setHeader('Content-Type', 'text/html; charset=%s' % charset)
+    setHeader('Content-Type', 'text/html;;charset=%s' % charset)
     setHeader('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT')
     setHeader('Last-Modified', 'Sat, 1 Jan 2000 00:00:00 GMT')
     setHeader('Cache-control', 'max-age=0,s-maxage=0,must-revalidate')
+    setHeader('Pragma', 'no-cache')
     return
 
