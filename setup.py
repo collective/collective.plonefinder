500fcf17602b7df625a056c9313dae83fd3168f3
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-04-24 17:45:27 +0200

    review packaging

--- setup.py
+++ setup.py
@@ -15,16 +15,26 @@ version = read('collective', 'plonefinder', 'version.txt')
 setup(name='collective.plonefinder',
       version=version,
       description="A finder to search/select portal objects for Plone",
-      long_description=(read("README.txt")
+      long_description=(read("README.rst")
                         + "\n\n"
                         +read("docs", "HISTORY.txt")),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-          'Framework :: Plone',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          ],
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='Zope Plone Medias Finder',
       author='Jean-mat Grimaldi / Alter Way Solutions',
       author_email='support@ingeniweb.com',
