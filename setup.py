d9d53be54dceb4ee8812136d547a6ae9e1edf47b
Godefroid Chapelle <gotcha@bubblenet.be>
2013-09-09 15:33:01 +0200

    Preparing release 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of collective.plonefinder
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.4.dev0'
+version = '1.0.4'
 
 setup(name='collective.plonefinder',
       version=version,
