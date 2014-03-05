cc43b488d7227e9d44aae1d43c3e1e74e376f535
Benoît Suttor <benoit.suttor@imio.be>
2014-03-05 10:10:03 +0100

    fix version, it seems that some changelog are gone.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = "1.0.4dev"
+version = "1.0.5dev"
 
 setup(name='collective.plonefinder',
       version=version,
