7b0c8ae5cd8ed1eaba9776c380b1edf29f3cc588
Benoît Suttor <benoit.suttor@imio.be>
2016-02-16 08:25:20 +0100

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = '1.1.0'
+version = '1.1.1.dev0'
 
 setup(name='collective.plonefinder',
       version=version,
