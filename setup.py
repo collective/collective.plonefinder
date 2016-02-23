e41903f7984a9ad99d6ca83e14718d5c52f6c3fa
Godefroid Chapelle <gotcha@bubblenet.be>
2016-02-23 09:37:31 +0100

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = '1.1.1'
+version = '1.1.2.dev0'
 
 setup(name='collective.plonefinder',
       version=version,
