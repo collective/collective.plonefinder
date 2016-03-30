bc14bb676ab11c119f5407aba15eeabf54cedf87
Gauthier Bastien <g.bastien@imio.be>
2016-03-30 11:03:49 +0200

    Preparing release 1.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = '1.1.4.dev0'
+version = '1.1.4'
 
 setup(name='collective.plonefinder',
       version=version,
