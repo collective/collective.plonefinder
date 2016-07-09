aea3458e9b8a520dc13882bcf4e5734235958309
Godefroid Chapelle <gotcha@bubblenet.be>
2016-07-09 12:36:31 +0200

    cleanup and comment

--- collective/plonefinder/browser/static/init_imagewidget.js
+++ collective/plonefinder/browser/static/init_imagewidget.js
@@ -7,26 +7,23 @@ initializeImageWidget = function(finder_url, initial_image_url, widget_id, input
     var parent = node.parentNode;
     elmapps[widget_id] = Elm.ImageWidget.embed(parent, [widget_id, input_id, initial_image_url]);    parent.removeChild(node);
     elmapps[widget_id].ports.openfinder.subscribe(function(widget_id) {
+        // widget_id is unused
+        // it is baked in finder_url
         openFinder(finder_url);
     });
-}
-
-finderSelectItem = function(selector, title, image_preview, widget_id) {
-    image_preview = (typeof image_preview != "undefined") ? image_preview : false;
-    if (image_preview) selector = selector + '/@@images/image/preview' ;
-    storeImageUrl(widget_id, selector);
-}
-
-storeImageUrl = function(widget_id, selector) {
-    if (selector === '') {
-      var image_url = '';
-    } else {
-      var image_url = portal_url + '/resolveuid/' + selector;
-    }
-    elmapps[widget_id].ports.url.send(image_url);
 }
 
 openFinder = function(url) {
     var finder_window = window.open(url,"plone_finder","menubar=no, status=no, scrollbars=no, menubar=no, width=980, height=650, left=20, top=20");
     finder_window.focus();
 }
+
+finderSelectItem = function(selector, title, image_preview, widget_id) {
+    image_preview = (typeof image_preview != "undefined") ? image_preview : false;
+    if (image_preview) selector = selector + '/@@images/image/preview' ;
+    // selector is the relative path from the portal
+    // compute absolute_url to be stored
+    var image_url = portal_url + '/resolveuid/' + selector;
+    elmapps[widget_id].ports.url.send(image_url);
+}
+
