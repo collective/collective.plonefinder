dea0a4da36bd60fe69e2c63bb1bfda9a1ac2458c
Godefroid Chapelle <gotcha@bubblenet.be>
2017-01-10 12:46:29 +0100

    Preparing release 1.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = '1.2.3.dev0'
+version = '1.2.3'
 
 setup(name='collective.plonefinder',
       version=version,
