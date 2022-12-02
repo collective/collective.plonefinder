1c6a2876272db77014a1eac32c3385a1b8674473
Kim Paulissen <kim.paulissen@kuleuven.be>
2022-12-02 14:28:52 +0100

    remove getSiteEncoding
    see https://github.com/plone/Products.CMFPlone/blob/5.2.7/Products/CMFPlone/utils.py#L218

--- collective/plonefinder/utils.py
+++ collective/plonefinder/utils.py
@@ -2,14 +2,11 @@
 # $Id$
 """Miscellaneous utilities"""
 
-from Products.CMFPlone.utils import getSiteEncoding
-
 def pleaseDontCache(context, request):
     """Requests the proxy (if any) and user browser to avoid caching the response
     """
-    charset = getSiteEncoding(context)
     setHeader = request.RESPONSE.setHeader
-    setHeader('Content-Type', 'text/html;;charset=%s' % charset)
+    setHeader('Content-Type', 'text/html;charset=utf-8')
     setHeader('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT')
     setHeader('Last-Modified', 'Sat, 1 Jan 2000 00:00:00 GMT')
     setHeader('Cache-control', 'max-age=0,s-maxage=0,must-revalidate')
